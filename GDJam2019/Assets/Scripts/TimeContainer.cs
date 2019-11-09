1b9efe154bcd28043fd1ed98cba334cd365390f1
Matt_Paras <matthew.paraskevakos@ontariotechu.net>
2019-11-09 02:39:19 -0500

    -Added base time powers

---
+++ GDJam2019/Assets/Scripts/TimeContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeContainer : MonoBehaviour
+{
+    [SerializeField]
+    float maxTime;
+    [SerializeField]
+    bool isRestored = false;
+
+    float _currentTime = 0;
+    // Start is called before the first frame update
+    private void Start()
+    {
+           _currentTime = isRestored ? maxTime : 0.0f ;
+    }
+    public void Drain(ref float  amount)
+    {
+        isRestored = false;
+        if (_currentTime - amount > 0)
+        {
+            _currentTime -= amount;
+
+        }
+        else if (_currentTime - amount <= 0)
+        {
+            amount =  _currentTime;
+            _currentTime = 0;
+        }
+    }
+    public void Restore( ref float amount)
+    {
+        if (_currentTime+amount<maxTime)
+        {
+            _currentTime += amount;
+
+        }else if (_currentTime+amount>=maxTime)
+        {
+            amount = (maxTime - _currentTime);
+            _currentTime = maxTime;
+            isRestored = true;
+        }
+    }
+}
---
+++ GDJam2019/Assets/Scripts/TimePower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePower : MonoBehaviour
+{
+    
+    [SerializeField]
+    float drainRestoreSpeed = 10f;
+
+    private float _currentTime;
+
+
+    public  string  GetCurrentTimeFormatted()
+    {
+        return  formatTime(_currentTime);
+    }
+
+    public void DrainObject(TimeContainer timeContainer)
+    {
+        float _drainAmount = Time.deltaTime* drainRestoreSpeed;
+        timeContainer.Drain(ref _drainAmount);
+        _currentTime += _drainAmount;
+    }
+    public void RestoreObject(TimeContainer timeContainer)
+    {
+        float _restoreAmount = Time.deltaTime * drainRestoreSpeed;
+        timeContainer.Restore(ref _restoreAmount);
+        _currentTime -= _restoreAmount;
+    }
+    public static string formatTime(double seconds)
+    {
+        System.TimeSpan timespan = System.TimeSpan.FromSeconds(seconds);
+        string mm = (timespan.Minutes).ToString("00");
+        string ss = (timespan.Seconds).ToString("00");
+        string ms = (timespan.Milliseconds).ToString("000");
+        return mm + ":" + ss + "." + ms;
+    }
+
+}
