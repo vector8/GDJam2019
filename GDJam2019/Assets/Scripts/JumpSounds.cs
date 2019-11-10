b375e54e7fff84f46de18c86c4528598190db459
Chris <cnj.carmichael@gmail.com>
2019-11-10 14:52:14 -0500

    Updated room. Added jump sounds.

---
+++ GDJam2019/Assets/Scripts/JumpSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class JumpSounds : MonoBehaviour
+{
+    public AudioClip[] benDeathSounds;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.clip = benDeathSounds[Random.Range(0, benDeathSounds.Length)];
+            audioSource.Play();
+        }
+    }
+}
